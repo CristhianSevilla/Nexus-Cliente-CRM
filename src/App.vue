5f048b7545ccd723e8fc3ae15221015d56e4e000
Cristhian Sevilla <crissev17@gmail.com>
2023-12-07 19:56:23 -0600

    Creando el Header

--- src/App.vue
+++ src/App.vue
@@ -3,5 +3,19 @@ import { RouterView } from "vue-router";
 </script>
 
 <template>
-  <RouterView />
+  <header class="bg-slate-950">
+    <div class="mx-auto px-5 xl:max-w-7xl py-7 flex items-center">
+      <img class="w-20 mr-5" src="/logo.svg" alt="Logo" />
+      <div>
+        <h1 class="text-4xl font-extrabold text-blue-200">
+          <span class="text-blue-800">Nexus</span>Client
+        </h1>
+        <p class="text-blue-200">Donde Cada Interacción Cuenta</p>
+      </div>
+    </div>
+  </header>
+
+  <main class="max-w-7xl mx-auto mt-10">
+    <RouterView />
+  </main>
 </template>
--- src/views/ClientesView.vue
+++ src/views/ClientesView.vue
@@ -1,7 +1,7 @@
 <script setup></script>
 
 <template>
-  <h1 class="text-6xl">Clientes</h1>
+  <h1 class="text-4xl font-extrabold text-blue-200 px-5">Clientes</h1>
 </template>
 
 <style scoped></style>
